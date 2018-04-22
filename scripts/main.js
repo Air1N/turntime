253f13606736738a908b783821ec531484608c45
UnknownEvil <reapor12@hotmail.com>
2018-04-22 11:17:37 -0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
-a
+var socket = io();
+
+let time = 0;
+let turn = true;
+
+function update() {
+    time++;
+    
+    if (time > 30 * 60) {
+        time = 0;
+        turn != turn;
+    }
+    
+    socket.emit('timeup', time);
+}
+
+socket.on('loadFile', function(data) {
+    time = data[0];
+    turn = data[1];
+});
+
+setInterval(update, 1000);
