613a7ecb74cf165dc9780f6a28c7390bc67f2567
UnknownEvil <reapor12@hotmail.com>
2018-04-22 11:12:39 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var port = process.env.PORT || 80;
 
 let time = 0;
+let turn = true;
 
 app.use('/', express.static(__dirname));
 app.get('/', function(req, res) {
@@ -13,30 +14,26 @@ app.get('/', function(req, res) {
 });
 
 function loadFiles() {
-  fs.readFile(__dirname + '/time.txt', 'utf8', function(err, data){
-    data = data.split('\n');
-    
-    io.emit('loadFile', data);
-  });
+    fs.readFile(__dirname + '/time.txt', 'utf8', function(err, data) {
+        data = data.split('||');
+
+        io.emit('loadFile', data);
+    });
 }
 
 function writeFile() {
-  fs.writeFile(__dirname + '/time.txt', time + "\n" + turn + "\n\n", function(err) {
-        if(err) {
-          return console.log(err);
-        }
-
+    fs.writeFile(__dirname + '/time.txt', time + "||" + turn, function(err) {
         console.log("The file was saved!");
-      });
+    });
 }
 
 io.on('connection', function(socket) {
     loadFiles();
-    
+
     socket.on('timeup', function(t) {
-       time = t;
+        time = t;
     });
-    
+
     socket.once('disconnect', function() {
         writeFile();
     });
