98462727f30afa494cf64105fcef6bda3e35f37d
UnknownEvil <reapor12@hotmail.com>
2018-04-22 11:58:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ var alarm = new Audio("./alarm.wav");
 function update() {
     time++;
     
-    if (time > 5) {
+    if (time > 1800) {
         time = 0;
         turn = !turn;
         
         alarm.play();
     }
     
-    timeleft.innerHTML = Math.floor((5 - time) / 60) + ":" + (5 - time) % 60;
+    timeleft.innerHTML = Math.floor((1800 - time) / 60) + ":" + (1800 - time) % 60;
     if (turn) {
         whosturn.innerHTML = "Sandra";
     } else {
