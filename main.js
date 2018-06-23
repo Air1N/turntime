0ea0b15234aed6a7d695b83575e26936caaa1dc4
UnknownEvil <reapor12@hotmail.com>
2018-06-23 12:27:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function update() {
     }
     
     timeleft.innerHTML = Math.floor((1800 - time) / 60) + ":" + (1800 - time) % 60;
-    whosturn.innerHTML = people[turn];
+    whosturn.innerHTML = people[turn] + "(" + turn + ")";
 }
 
 socket.on('loadFile', function(data) {
