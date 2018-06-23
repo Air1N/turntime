aa790e38bc099b5e5217025993c18787780a2189
UnknownEvil <reapor12@hotmail.com>
2018-06-23 12:12:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ var alarm = new Audio("./alarm.wav");
 
 function update() {
     if (time > 1800) {
-        //alarm.play();
+        alarm.play();
     }
     
     timeleft.innerHTML = Math.floor((1800 - time) / 60) + ":" + (1800 - time) % 60;
-    whosturn.innerHTML = "Turn: " + turn;
+    whosturn.innerHTML = "Turn " + turn;
 }
 
 socket.on('loadFile', function(data) {
