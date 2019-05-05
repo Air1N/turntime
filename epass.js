c0815ab5e7a82d5d372c16d8c359147078e0fdd2
Aaron Denny <reapor12@hotmail.com>
2019-05-05 19:16:40 -0400

    Update epass.js

--- epass.js
+++ epass.js
@@ -1,2 +1,9 @@
 let password = "";
 let passwords = ["113", "412"];
+
+while (window.password != passwords[turn]) {
+  alarm.play();
+
+  window.password = prompt("Enter your password " + people[!turn ? 1 : 0]);
+  alert("Nope! Try again");
+}
